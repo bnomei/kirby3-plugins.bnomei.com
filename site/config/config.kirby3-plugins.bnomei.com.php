1c3f7d562adcd414d8689912f30578099a99e3b6
Bruno Meilick <b@bnomei.com>
2019-02-06 13:38:16 +0100

    cloudconvert
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- site/config/config.kirby3-plugins.bnomei.com.php
+++ site/config/config.kirby3-plugins.bnomei.com.php
@@ -1,6 +1,38 @@
 <?php
 
+function customConvertHook($file) {
+    if($file->extension() == 'gif') {
+
+        $file->cloudconvert(
+            [
+                'inputformat' => 'gif',
+                'outputformat' => 'webm',
+                'save' => true, // keep file at cloud to avoid another download from cloud-server
+                'converteroptions' => [
+                    // https://stackoverflow.com/questions/45588064/error-transparency-encoding-with-auto-alt-ref-does-not-work-when-converting-a#45588400
+                    'command' => '-i {INPUTFILE} {OUTPUTFILE} -auto-alt-ref 0',
+                ],
+            ]
+        );
+
+        $file->cloudconvert(
+            [
+                'inputformat' => 'gif',
+                'outputformat' => 'mp4',
+            ]
+        );
+    }
+}
 
 return [
-    'debug' => false
+    'debug' => false,
+
+    'hooks' => [
+        'file.create:after' => function($file) {
+            customConvertHook($file);
+        },
+        'file.replace:after' => function($newFile, $oldFile) {
+            customConvertHook($newFile);
+        },
+    ]
 ];
---
+++ site/templates/cloudconvert.php
@@ -0,0 +1,64 @@
+<?php
+/** @var Kirby\Cms\App $kirby */
+/** @var Kirby\Cms\Site $site */
+/** @var Kirby\Cms\Page $page */
+?><!DOCTYPE html>
+<html <?= site()->attrLang() ?>>
+<head><?php snippet('plugin-htmlhead') ?></head>
+<body>
+<?php
+$github = lapse(md5($page->id()), function() {
+  return [
+    'templates/cloudconvert' => html(Kirby\Http\Remote::get('https://raw.githubusercontent.com/bnomei/kirby3-plugins.bnomei.com/master/site/templates/cloudconvert.php')->content()),
+    'config/config.kirby3-plugins.bnomei.com' => html(Kirby\Http\Remote::get('https://raw.githubusercontent.com/bnomei/kirby3-plugins.bnomei.com/master/site/config/config.kirby3-plugins.bnomei.com')->content()),
+  ];
+}); ?>
+<pre><code><?= $github['templates/cloudconvert'] ?></code></pre>
+
+<b>site/config/config.kirby3-plugins.bnomei.com.php</b>
+<pre><code><?= $github['config/config.kirby3-plugins.bnomei.com'] ?></code></pre>
+
+<p>
+<b>synchronously docx to pdf</b><br>
+<?php
+if ($fileWord = $page->file('test.docx')) {
+  $filePDF = $fileWord->cloudconvert(
+    [
+      'inputformat' => 'docx',
+      'outputformat' => 'pdf',
+    ],
+    str_replace('.docx', '.pdf', $fileWord->root()),
+    false // wait for conversion to be done
+  );
+  echo $fileWord->url().PHP_EOL;
+  if ($filePDF) {
+    echo $filePDF->url().PHP_EOL;
+  }
+}
+?>
+</p>
+
+<p>
+<b>asynchronously jpg to webp</b><br>
+<?php
+if ($fileJpg = $page->file('flowers.jpg')) {
+  echo $fileJpg->url().PHP_EOL;
+  $process = $fileJpg->cloudconvert(
+    [
+      'inputformat' => 'jpg',
+      'outputformat' => 'webp',
+    ],
+    str_replace('.jpg', '.webp', $fileJpg->root())
+  // NOTICE: no 'false' here!
+  );
+}
+// NOTICE: file might not exists immediately
+if ($fileWebp = $page->file('flowers.webp')) {
+  echo $fileWebp->url().PHP_EOL;
+} else {
+  echo 'does not exist yet.'.PHP_EOL;
+}
+?>
+</p>
+</body>
+</html>
