e2641a64a804412621094efa91370ce857ef6c5b
Bruno Meilick <b@bnomei.com>
2019-08-16 13:55:51 +0200

    new .env path
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- site/config/config.k3p.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = require_once(__DIR__ . '/config.localhost.php');
-
-return array_merge($config, [
-    'debug' => true,
-]);
--- site/config/config.php
+++ site/config/config.php
@@ -5,7 +5,7 @@ return [
     'debug' => true,
 
     'bnomei.dotenv.dir' => function () {
-        return realpath(kirby()->roots()->index() . '/../');
+        return realpath(kirby()->roots()->index() . '/../../../');
     },
 
     'bnomei.instagram.params' => [
