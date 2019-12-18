1a52f694a292a466e1df30b7f21d1d673fedcde4
Bruno Meilick <b@bnomei.com>
2019-12-18 23:48:34 +0100

    :sparkles: issues & codequality table
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- site/templates/home.php
+++ site/templates/home.php
@@ -19,6 +19,48 @@
     <?php endforeach; ?>
   </ul>
 
+  <h2>Issues & Code Quality</h2>
+  <table class="table--readable mb-12">
+    <thead>
+      <tr>
+        <td>Repository</td>
+        <td>Release</td>
+        <td>Stars</td>
+        <td>Downloads</td>
+        <td>Issues</td>
+        <td>Build Status</td>
+        <td>Coverage Status</td>
+        <td>Maintainability</td>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+    $plugins = [
+      'autoid', 'lapse', 'security-headers', 'janitor', 'redis-cachedriver',
+      'mailjet', 'stopwatch', 'ics', 'monolog', 'qrcode', 'feed', 'srcset',
+      'fingerprint', 'field-ecco', 'cloudconvert', 'doctor', 'dotenv', 'instagram',
+      'mobile-detect', 'random', 'redirects', 'robots-txt', 'htmlhead', 'handlebars',
+      'thumb-imageoptim', 'csv',
+      // 'calendar',
+      // 'digitalocean-spaces-cdn',
+    ];
+    sort($plugins);
+
+    foreach ($plugins as $plugin): ?>
+    <tr>
+      <td><a href="https://github.com/bnomei/kirby3-<?= $plugin ?>">kirby3-<?= $plugin ?></a></td>
+      <td><img src="https://flat.badgen.net/packagist/v/bnomei/kirby3-<?= $plugin ?>?color=ae81ff"></td>
+      <td><img src="https://flat.badgen.net/packagist/ghs/bnomei/kirby3-<?= $plugin ?>?color=272822"></td>
+      <td><img src="https://flat.badgen.net/packagist/dt/bnomei/kirby3-<?= $plugin ?>?color=272822"></td>
+      <td><img src="https://flat.badgen.net/packagist/ghi/bnomei/kirby3-<?= $plugin ?>?color=e6db74"></td>
+      <td><a href==https://travis-ci.com/bnomei/kirby3-<?= $plugin ?>"><img src="https://flat.badgen.net/travis/bnomei/kirby3-<?= $plugin ?>"></a></td>
+      <td><a href==https://coveralls.io/github/bnomei/kirby3-<?= $plugin ?>"><img src="https://flat.badgen.net/coveralls/c/github/bnomei/kirby3-<?= $plugin ?>"></a></td>
+      <td><a href==https://codeclimate.com/github/bnomei/kirby3-<?= $plugin ?>)"><img src="https://flat.badgen.net/codeclimate/maintainability/bnomei/kirby3-<?= $plugin ?>"></a></td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+  </table>
+
   <?php snippet('footer') ?>
 </body>
 </html>
